44dd2c54e9fde44bfc11697b9252eac14ef5b8f9
ridafkih <hello@rida.dev>
2025-04-25 18:27:33 -0400

    Allow parsing and hashing contents

--- index.ts
+++ index.ts
@@ -1,1 +1,13 @@
-console.log("Hello via Bun!");
+import xxhash from "@ridafkih/xxhash-wasm"
+import { isUrlValid } from "./utils/url";
+
+const [, , url] = process.argv;
+
+if (!isUrlValid(url)) {
+  throw Error(`An invalid URL '${url}' was passed.`)  
+}
+
+const response = await fetch(url, { method: "GET" })
+  .then((value) => value.text())
+  
+console.log(xxhash().h32ToString(response));
