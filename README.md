c0f409e96a6c768cba76deff644073d45498390a
ridafkih <hello@rida.dev>
2025-04-25 19:48:44 -0400

    Add redis logic

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 # olev
 
+## Prerequisites
+
+In order to compare the hash of the current job postings with that which is stored in the Redis store, you need to instantiate a Redis store, then set the `REDIS_URL` environment variable to the url which points to the Redis store. If you're part of the Vercel organization which is linked to a Redis storage, you can run the following command.
+
+```bash
+vercel env pull .env.development.local
+```
+
 ## Recruiter Platform Scraping Tool
 
 This is a tool that allows you to scrape data from recruitment platforms.
--- lever.ts
+++ lever.ts
@@ -1,11 +1,17 @@
+import redis from "redis";
 import xxhash from "@ridafkih/xxhash-wasm"
 import { isUrlValid } from "./utils/url";
 import { Lever } from "./platforms/lever";
 
+const { REDIS_URL } = process.env;
 const [, , url] = process.argv;
 
 if (!isUrlValid(url)) {
   throw Error(`An invalid URL '${url}' was passed.`)  
+}
+
+if (!isUrlValid(REDIS_URL)) {
+  throw Error(`An invalid \`REDIS_URL\` '${REDIS_URL}' was passed.`)
 }
 
 const hash = xxhash().create32()
@@ -13,4 +19,31 @@ const hash = xxhash().create32()
 const lever = new Lever(url)
 const listings = await lever.getListings();
 
-console.log(listings);
+for (const listing of listings) {
+  const { id, title } = listing;
+  hash.update(id + title);
+}
+
+const digest = hash.digest().toString(16)
+
+const store = await redis.createClient({ url: REDIS_URL }).connect()
+const storedHash = await store.get(url)
+
+if (storedHash === null || storedHash !== digest) {
+  console.log([
+    "⚠️",
+    `'${storedHash}' was stored and failed the check against '${digest}'.`,
+    `This hash was generated based off the contents of ${listings.length} listings on '${url}'.`,
+    "Saving new value and notifying."
+  ].join("\n"))
+
+  await store.set(url, digest);
+} else {
+  console.log([
+    `'${storedHash}' was stored and passed the check against '${digest}'.`,
+    `This hash was generated based off the contents of ${listings.length} listings on '${url}'.`,
+    "No changes or notifications needed."
+  ].join("\n"))
+}
+
+await store.disconnect();
