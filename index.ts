848b4b57974264c0edf26a6fd75a8e97b8abea65
ridafkih <hello@rida.dev>
2025-04-25 19:22:03 -0400

    Cleanup main index file

--- index.ts
+++ index.ts
@@ -8,11 +8,6 @@ if (!isUrlValid(url)) {
   throw Error(`An invalid URL '${url}' was passed.`)  
 }
 
-const response = await fetch(url, { method: "GET" })
-  .then((value) => value.text())
-  
-console.log(xxhash().h32ToString(response));
-
 const lever = new Lever(url)
 const listings = await lever.getListings();
 
