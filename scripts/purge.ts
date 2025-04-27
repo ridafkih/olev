6e4cda31fa7b453e20212a232d028d753664f6b5
ridafkih <hello@rida.dev>
2025-04-27 16:01:43 -0400

    Update purge command

--- scripts/purge.ts
+++ scripts/purge.ts
@@ -1,6 +1,6 @@
 import { type } from "arktype";
-import { EnvironmentVariableManager } from "../src/modules/EnvironmentVariableManager";
-import { RedisClient } from "../src/modules/RedisClient";
+import { EnvironmentVariableManager } from "../src/modules/environment";
+import { Redis } from "../src/modules/redis";
 
 const {
   REDIS_URL
@@ -8,7 +8,7 @@ const {
   REDIS_URL: 'string.url',
 })).getAll()
 
-const redisClient = new RedisClient(REDIS_URL);
+const redisClient = new Redis(REDIS_URL);
 await redisClient.start();
 await redisClient.purge();
 await redisClient.stop();
